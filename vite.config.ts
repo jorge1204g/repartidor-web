1ce14394c693e3d36ca7d11ac2ccc86367da34bc
jorge1204g <127909660+jorge1204g@users.noreply.github.com>
2026-02-17 15:16:45 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
   server: {
     port: 3000,
@@ -11,11 +10,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
+    sourcemap: true
   }
 })
